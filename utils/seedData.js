bdb7ff343706191e4d5e36c509cc876be819669a
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-21 23:53:11 -0500

    seed data created

---
+++ utils/seedData.js
@@ -0,0 +1,18 @@
+
+const user = [
+    { username: 'imBatman', email: 'wayne.bruce@wayneindustries.com' },
+    { username: 'superman1337', email: 'kent.clark@dailyplanet.com' },
+    { username: 'SmashThePatriarchy', email: 'prince.diana@themyscara.gov' },
+    { username: 'footloose', email: 'allen.barry@ccpd.gov' },
+    { username: 'webhead', email: 'peterbparker@dailybugle.biz' },
+  ]
+  
+  const thoughts = [
+    { thoughtText: "What good is having god like powers if your movies can't make that Bruce Wayne money?  Apparently I do have a super power, the Power of Profitability! #backpain #carryingtheDCU" , username: 'imBatman' },
+    { thoughtText: "Hey kids!  Don't forget, say your prayers and eat your vitamins so you can be super too!", username: 'superman1337' },
+    { thoughtText: "Chose letting my ancient aunt's life over not only my marriage, but every memory from my relationship with my wife.  AITA?", username: 'webhead' },
+  ]
+  
+  
+  
+  module.exports = { user, thoughts };
