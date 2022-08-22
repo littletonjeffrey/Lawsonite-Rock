9096026b49946e12ead273c18a7e18b44f243a53
Jeffrey Littleton <littletonjeffrey@gmail.com>
2022-08-21 19:54:09 -0500

    seed.js made and filled out, not tested

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,5 +1,6 @@
 //exports the helper function
 module.exports = {
+    //formats date to be in mm/dd/yyyy format
     dateFormat: (currentDate) => {
         return `${new Date(currentDate).getMonth() + 1}
         /
---
+++ utils/seed.js
@@ -0,0 +1,27 @@
+const connection = require('../config/connection');
+const { Thought, User } = require('../models');
+
+connection.on('error', (err) => err);
+
+connection.once('open', async () => {
+  console.log('connected');
+
+  // Drop existing thoughts
+  await Thought.deleteMany({});
+
+  // Drop existing Users
+  await User.deleteMany({});
+
+
+// Add Users to the collection and await the results
+  await User.collection.insertMany(Users);
+
+  // Add Thoughts to the collection and await the results
+  await Thought.collection.insertMany(Thought);
+
+  // Log out the seed data to indicate what should appear in the database
+  console.table(User);
+  console.table(Thought);
+  console.info('Seeding complete! 🌱');
+  process.exit(0);
+});
