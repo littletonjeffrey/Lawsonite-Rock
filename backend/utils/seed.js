92650414ca5d6c36e72eca8c003c1ed126d8d327
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-22 18:01:08 -0500

    new seed error

--- backend/utils/seed.js
+++ backend/utils/seed.js
@@ -1,11 +1,12 @@
-const connection = require('../config/connection');
-const { Thought, User } = require('../models');
-const { user, thought } = require('./seedData');
+const connection = require("../config/connection");
+const { Thought, User } = require("../models");
+const { user, thought } = require("./seedData");
+const colors = require("colors");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
+connection.once("open", async () => {
+  console.log("CONNECTED TO DB".bgGreen);
 
   // Drop existing thoughts
   await Thought.deleteMany({});
@@ -13,16 +14,15 @@ connection.once('open', async () => {
   // Drop existing Users
   await User.deleteMany({});
 
-
-// Add Users to the collection and await the results
-  await User.collection.insertMany(User);
+  // Add Users to the collection and await the results
+  await User.insertMany({ user });
 
   // Add Thoughts to the collection and await the results
-  await Thought.collection.insertMany(Thought);
+  await Thought.insertMany({ thought });
 
   // Log out the seed data to indicate what should appear in the database
   console.table(User);
   console.table(Thought);
-  console.info('Seeding complete! 🌱');
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
