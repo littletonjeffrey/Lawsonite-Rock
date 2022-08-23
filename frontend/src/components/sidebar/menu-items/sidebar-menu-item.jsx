9dc8525971b815d4ca185935e74139809d64b16d
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-22 19:44:01 -0500

    sidebar on page!

--- frontend/src/components/sidebar/menu-items/sidebar-menu-item.jsx
+++ frontend/src/components/sidebar/menu-items/sidebar-menu-item.jsx
@@ -1,11 +1,10 @@
 export default function MenuItem({ text, Icon, src }) {
   return (
-      <a href={src}>
-        <button className="hover:scale-105 flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3">
-          <Icon className="h-7 w-16 align-middle" />
-          <span className="-ml-3 align-middle">{text}</span>
-        </button>
-      </a>
+    <a href={src}>
+      <button className="hover:scale-105 flex text-white text-lg mb-2 w-36">
+        <Icon className="h-7 w-16 -ml-5" />
+        <span className="-ml-1">{text}</span>
+      </button>
+    </a>
   );
 }
-
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import Sidebar from "./components/sidebar/Sidebar";
 import "./styles/index.css";
+import { Page } from "./components/templates/Page";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Page currentPage="Home">
+
     </Page>
   </React.StrictMode>
 );
