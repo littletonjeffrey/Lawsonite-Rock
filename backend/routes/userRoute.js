87e3d5a1563cb4c0404ad2375381744f40a34ac7
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-08-28 15:59:55 -0500

    add friend works

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,19 @@
-const { getUsers, getSingleUser, createUser, deleteUser, addFriend, removeFriend } = require("../controller/userController");
-const express = require('express');
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  updateUser,
+} = require("../controller/userController");
+const express = require("express");
 const router = express.Router();
 
+router.route("/").get(getUsers).post(createUser);
 
-router.route('/').get(getUsers).post(createUser)
+router.route("/:id").get(getSingleUser).delete(deleteUser).put(updateUser);
 
-router.route('/:id').get(getSingleUser).delete(deleteUser).put(addFriend).put(removeFriend)
+router.route("/:id/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
